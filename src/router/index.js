f2985db0c610fed4b6a06102cabbc9c3e8a1ced4
Rain1227 <1262208607@qq.com>
2019-11-13 20:23:38 +0800

    头条初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/home' // 简写路径
+import Login from '../views/login'
 Vue.use(VueRouter)
 
 const routes = [{
   path: '/',
+  redirect: '/home'
+}, {
+  // 一级路由 主页
+  path: '/home',
   name: 'home',
   component: Home
+},
+{ // 一级路由  登录页面
+  path: '/login',
+  component: Login
 }
   // {
   //   path: '/about',
